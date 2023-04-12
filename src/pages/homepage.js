cb9a03fd0fe858f21f077518ee52afa17598a40c
Rubbed_Genie <matt.daly7@gmail.com>
2023-04-12 14:35:23 +1000

    added old website

---
+++ src/pages/homepage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import IndexPage from "../components/indexPage"
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <IndexPage />
+    </Layout>
+  )
+}
+
+export default HomePage
